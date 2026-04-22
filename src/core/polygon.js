e0df541f15a709ff813a5ecb76580b9c1fa84c73
nocapkenny <blestkenny@yandex.ru>
2026-04-22 19:45:31 +0500

    fix: change geometry of polygon

--- src/core/polygon.js
+++ src/core/polygon.js
@@ -1,7 +1,7 @@
 import { clampPointToCanvas, rand, randomColor } from "../utils/helpers.js";
 
 /**
- * 
+ *
  * @param {number} canvasWidth - Ширина канваса
  * @param {number} canvasHeight - Высота канваса
  * @returns {{ id: number, points: [{x: number, y: number}], color: string }} - Полигон
@@ -15,14 +15,23 @@ export const generatePolygon = (canvasWidth, canvasHeight) => {
 
   const points = [];
 
+  const shouldBeConcave = Math.random() < 0.5;
+  const concaveIndices = shouldBeConcave ? [rand(0, pointsCount)] : [];
+
   for (let i = 0; i < pointsCount; i++) {
     const angle = (Math.PI * 2 * i) / pointsCount;
-    const radius = rand(30, 70);
+    let radius = rand(35, 75);
 
-    points.push(clampPointToCanvas(canvasWidth, canvasHeight, {
-      x: cx + Math.cos(angle) * radius,
-      y: cy + Math.sin(angle) * radius,
-    }));
+    if (concaveIndices.includes(i)) {
+      radius = rand(10, 28);
+    }
+
+    points.push(
+      clampPointToCanvas(canvasWidth, canvasHeight, {
+        x: cx + Math.cos(angle) * radius,
+        y: cy + Math.sin(angle) * radius,
+      }),
+    );
   }
 
   return {
